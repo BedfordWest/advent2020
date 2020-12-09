2f28b07d71d49c46d583e0b77f41a07be1fcbc0b
Bedford West <bedfordwest@gmail.com>
2020-12-08 19:36:36 -0600

    -Solve pt2

--- src/day4/solution.js
+++ src/day4/solution.js
@@ -17,7 +17,7 @@ const solve = async function(part) {
 const solve1 = function(inputArray) {
   var valid = 0
   inputArray.forEach(passport => {
-    if(checkValidity(passport)) {
+    if(checkValidity(passport, 1)) {
       valid++
     }
   })
@@ -25,38 +25,131 @@ const solve1 = function(inputArray) {
 }
 
 const solve2 = function(inputArray) {    
-  console.log(inputArray)
+  var valid = 0
+  inputArray.forEach(passport => {
+    if(checkValidity(passport, 2)) {
+      valid++
+    }
+  })
+  return valid
 }
 
-const checkValidity = function(passport) {
+const checkValidity = function(passport, part) {
   var entries = passport.split(/[\s]+/)
 
   var entryCounts = {
-    byr: 0,
-    iyr: 0,
-    eyr: 0,
-    hgt: 0,
-    hcl: 0,
-    ecl: 0,
-    pid: 0,
-    cid: 0
+    byr: {count: 0, actionFunc: byrValidity},
+    iyr: {count: 0, actionFunc: iyrValidity},
+    eyr: {count: 0, actionFunc: eyrValidity},
+    hgt: {count: 0, actionFunc: hgtValidity},
+    hcl: {count: 0, actionFunc: hclValidity},
+    ecl: {count: 0, actionFunc: eclValidity},
+    pid: {count: 0, actionFunc: pidValidity},
+    cid: {count: 0, actionFunc: cidValidity},
   }
 
-  entries.forEach(entry => {
-    var key = entry.split(":")[0]
-    entryCounts[key]++
-  })
+  const entryValid = function(key, value) {
+    return entryCounts[key]["actionFunc"](value)
+  }
+
+  for (var entry of entries) {
+    var splitEntry = entry.split(":")
+    const key = splitEntry[0]
+    const value = splitEntry[1]
+    entryCounts[key]["count"]++
+    if(part == 2) {
+      if(!entryValid(key, value)) {
+        return false
+      }
+    }
+  }
 
   var filtered = _.pickBy(entryCounts, function(val) {
-    return val < 1;
+    return val["count"] < 1;
   })
 
-  console.log(filtered)
-
   if(Object.keys(filtered).length > 1) { return false }
-  if((Object.keys(filtered).length == 1) && (entryCounts["cid"] > 0)) { return false }
+  if((Object.keys(filtered).length == 1) && (entryCounts["cid"]["count"] > 0)) { return false }
   return true;
+}
 
+const byrValidity = function(entry) {
+  if(!isNumeric(entry)) { return false }
+  entry = parseInt(entry)
+  if((entry < 1920) || (entry > 2002)) { return false }
+  return true
+}
+
+const iyrValidity = function(entry) {
+  if(!isNumeric(entry)) { return false }
+  entry = parseInt(entry)
+  if((entry < 2010) || (entry > 2020)) { return false }
+  return true  
+}
+
+const eyrValidity = function(entry) {
+  if(!isNumeric(entry)) { return false }  
+  entry = parseInt(entry)
+  if((entry < 2020) || (entry > 2030)) { return false }
+  return true  
+}
+
+const hgtValidity = function(entry) {
+  if(entry.length < 3) { return false }
+  entry = entry.split('')
+  var type = []
+  type.push(entry.pop())
+  type.push(entry.pop())
+  type.reverse()
+
+  type = type.join('')
+  entry = entry.join('')
+
+  if(!isNumeric(entry)) { return false }
+  entry = parseInt(entry)
+
+  if(type == "cm") {
+    if((entry < 150) || (entry > 193)) { return false }
+    return true
+  }
+  else if(type == "in") {
+    if((entry < 59) || (entry > 76)) { return false }
+    return true
+  }
+  return false
+}
+
+const hclValidity = function(entry) {
+  if(entry.length != 7) { return false }
+  entry = entry.split('')
+
+  if(entry.shift() != '#') { return false }
+  const valid = "abcdefg0123456789"
+  entry.forEach(char => {
+    if(!valid.includes(char)) { return false }
+  })
+  return true
+}
+
+const eclValidity = function(entry) {
+  const valid = "amb blu brn gry grn hzl oth".split(" ")
+  if(!valid.includes(entry)) { return false }
+  return true
+}
+
+const pidValidity = function(entry) {
+  if(!isNumeric(entry)) { return false }
+  if(entry.length != 9) { return false }
+  entry = parseInt(entry)
+  return true
+}
+
+const cidValidity = function(entry) {
+  return true  
+}
+
+const isNumeric = function(value) {
+  return /^-?\d+$/.test(value);
 }
 
 export default solve;
