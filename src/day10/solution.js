a07b8694b80a7c6e3241d8d2634aba43f888317c
Bedford West <bedfordwest@gmail.com>
2020-12-13 00:06:11 -0600

    -Solve pt2

--- src/day10/solution.js
+++ src/day10/solution.js
@@ -3,6 +3,10 @@ const solve = async function(part) {
 
   const inputs = file.inputs
   var inputArray = inputs.split("\n\r")
+  inputArray.sort((a, b) => a - b)
+  inputArray = inputArray.map((el) => parseInt(el))
+  inputArray.push(inputArray[inputArray.length - 1] + 3)
+  inputArray.unshift(0)
 
   if(part == 1) {
       return solve1(inputArray)
@@ -15,9 +19,6 @@ const solve = async function(part) {
 const solve1 = function(inputArray) {
   let oneDiff = 0
   let threeDiff = 0
-  inputArray.sort((a, b) => a - b)
-  inputArray = inputArray.map((el) => parseInt(el))
-  inputArray.push(inputArray[inputArray.length - 1] + 3)
   inputArray.forEach((adapter, i) => {
     let diff = 0
     if(i == 0) { 
@@ -35,9 +36,42 @@ const solve1 = function(inputArray) {
 }
 
 const solve2 = function(inputArray) {
+  const skips = []
   console.log(inputArray)
+  inputArray.forEach((adapter, i) => {
+    var diff = 0
+    if(i != 0) {
+      diff = adapter - inputArray[i - 1]
+      skips.push(diff)
+    }
+  })
+
+  var numOnes = []
+  var oneCounter = 0
+  for(let j = 0; j < skips.length; j++) {
+    if(skips[j] == 1) {
+      oneCounter++
+    } else {
+      numOnes.push(oneCounter)
+      oneCounter = 0
+    }
+  }
+
+  let total =  numOnes.reduce((acc,curr) => {  
+    if(curr < 2) {
+      return acc
+    } else if(curr == 2) {
+      return acc * 2
+    }
+    else if(curr == 3) {
+      return acc * 4
+    }
+    else if(curr == 4) {
+      return acc * 7
+    }
+  }, 1)
+  return total
+  
 }
-
-
 
 export default solve;
