9c0754d9376617916104bd4b5d2aca745c542460
Bedford West <bedfordwest@gmail.com>
2020-12-14 00:40:31 -0600

    -Solve pt2

--- src/day12/solution.js
+++ src/day12/solution.js
@@ -1,8 +1,12 @@
 var facing = 0
 var position = [0,0]
+var wayPosition = [10,1]
 
 const solve = async function(part) {
   const file = require(`../day12/INPUTS.json`)
+  facing = 0
+  position = [0,0]
+  wayPosition = [10,1]
 
   const inputs = file.inputs
   var inputArray = inputs.split("\n\r")
@@ -23,8 +27,13 @@ const solve1 = function(inputArray) {
   return Math.abs(position[0]) + Math.abs(position[1])
 }
 
-const solve2 = function(inputArray) {    
-  console.log(inputArray)
+const solve2 = function(inputArray) {
+  facing = getAngle()
+  inputArray.forEach(instruction => {
+    let command = parseInstruction(instruction)
+    performWPAction(command)
+  })
+  return Math.abs(position[0]) + Math.abs(position[1])
 }
 
 const parseInstruction = function(instruction) {
@@ -38,10 +47,10 @@ const performAction = function(instruction) {
   let command = instruction[0]
   let value = instruction[1]
   switch(command) {
-    case 'R':
+    case 'L':
       facing = (facing + value) % 360
       break
-    case 'L':
+    case 'R':
       facing = (facing - value) % 360
       if(facing < 0) {
         facing = 360 + facing
@@ -81,6 +90,59 @@ const performAction = function(instruction) {
       position[0] -= value
       break
     }
+}
+
+const updateWayPosition = function() {
+    let mag = Math.hypot(wayPosition[0], wayPosition[1])
+    let radFacing = facing * (Math.PI/180)
+    wayPosition[0] = mag * Math.cos(radFacing)
+    wayPosition[1] = mag * Math.sin(radFacing)
+}
+
+const performWPAction = function(instruction) {
+  let command = instruction[0]
+  let value = instruction[1]
+  switch(command) {
+    case 'L':
+      facing = (facing + value) % 360
+      updateWayPosition()
+      break
+    case 'R':
+      facing = (facing - value) % 360
+      if(facing < 0) {
+        facing += 360
+      }
+      updateWayPosition()
+      break
+    case 'F':
+      position[0] += value * wayPosition[0]
+      position[1] += value * wayPosition[1]
+      break
+    case 'N':
+      wayPosition[1] += value
+      facing = getAngle()
+      break
+    case 'E':
+      wayPosition[0] += value
+      facing = getAngle()
+      break
+    case 'S':
+      wayPosition[1] -= value
+      facing = getAngle()
+      break
+    case 'W':
+      wayPosition[0] -= value
+      facing = getAngle()
+      break
+    }
+}
+
+const getAngle = function() {
+  let angle = Math.atan2(wayPosition[1], wayPosition[0]) * (180 / Math.PI);
+  if(angle < 0) {
+    angle += 360
+  }
+  return angle
 }
 
 export default solve;
