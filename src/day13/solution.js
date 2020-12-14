98fe0d825a64c5fd99b38e1bda9be8f0782b247e
Bedford West <bedfordwest@gmail.com>
2020-12-14 09:53:57 -0600

    -Solve pt1

--- src/day13/solution.js
+++ src/day13/solution.js
@@ -3,19 +3,33 @@ const solve = async function(part) {
 
   const inputs = file.inputs
   var inputArray = inputs.split("\n\r")
-  const earliest = inputArray[0]
-  const buses = inputArray[1].split(',').filter((bus) => bus != 'x')
+  const earliest = parseInt(inputArray[0])
+  const buses = inputArray[1]
+    .split(',')
+    .filter((bus) => bus != 'x')
+    .map((bus) => parseInt(bus))
+    .sort((a, b) => a - b)
 
   if(part == 1) {
-      return solve1(inputArray)
+      return solve1(earliest, buses)
   }
   else {
       return solve2(inputArray)
   }
 }
 
-const solve1 = function(inputArray) {
-  console.log(buses)
+const solve1 = function(earliest, buses) {
+  var found = 0
+  var minutes = -1
+  while(!found) {
+    minutes++
+    buses.forEach(bus => {
+      if(((earliest + minutes) % bus) == 0) {
+        found = bus
+      }
+    })
+  }
+  return (minutes * found)
 }
 
 const solve2 = function(inputArray) {    
