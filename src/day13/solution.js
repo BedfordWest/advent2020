82b2a90edba31e97f9ec19b3251f012deef64ef4
Bedford West <bedfordwest@gmail.com>
2020-12-14 21:45:10 -0600

    -Solve pt2

--- src/day13/solution.js
+++ src/day13/solution.js
@@ -3,22 +3,22 @@ const solve = async function(part) {
 
   const inputs = file.inputs
   var inputArray = inputs.split("\n\r")
-  const earliest = parseInt(inputArray[0])
-  const buses = inputArray[1]
-    .split(',')
-    .filter((bus) => bus != 'x')
-    .map((bus) => parseInt(bus))
-    .sort((a, b) => a - b)
 
   if(part == 1) {
-      return solve1(earliest, buses)
+      return solve1(inputArray)
   }
   else {
       return solve2(inputArray)
   }
 }
 
-const solve1 = function(earliest, buses) {
+const solve1 = function(inputArray) {
+  let earliest = parseInt(inputArray[0])
+  let buses = inputArray[1]
+    .split(',')
+    .filter((bus) => bus != 'x')
+    .map((bus) => parseInt(bus))
+    .sort((a, b) => a - b)
   var found = 0
   var minutes = -1
   while(!found) {
@@ -32,10 +32,37 @@ const solve1 = function(earliest, buses) {
   return (minutes * found)
 }
 
-const solve2 = function(inputArray) {    
-  console.log(inputArray)
+const solve2 = function(inputArray) {  
+  let buses = inputArray[1]
+    .split(',')
+
+  let originalBuses = buses
+  .map((bus, index) => {
+    if(bus == 'x') {
+      return null
+    } else {
+      return [BigInt(parseInt(bus)), BigInt(index + 1)]
+    }
+  })
+  .filter((bus) => bus != null)
+
+
+
+  let found = 0n
+  let lcm = originalBuses[0][0]
+  let minutes = lcm - originalBuses[0][1]
+  for(let i = 1; i < originalBuses.length; i++) {
+    while(true) {
+      minutes += lcm      
+      if((minutes + originalBuses[i][1]) % originalBuses[i][0] != 0) {
+        continue
+      } else {
+        lcm *= originalBuses[i][0]
+        break
+      }
+    }
+  }
+  return minutes + 1n
 }
-
-
 
 export default solve;
