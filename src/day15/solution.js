91ad9b72e321637c96c561c41548704226c463a2
Bedford West <bedfordwest@gmail.com>
2020-12-16 22:43:25 -0600

    -Solve pt2

--- src/day15/solution.js
+++ src/day15/solution.js
@@ -1,8 +1,14 @@
+var foundAts = {}
+
 const solve = async function(part) {
   const file = require(`../day15/INPUTS.json`)
 
   const inputs = file.inputs
   var inputArray = inputs.split(",").map((el) => parseInt(el))
+  foundAts = {}
+  for(let i = 0; i < inputArray.length - 1; i++) {
+    foundAts[inputArray[i]] = i
+  }
 
   if(part == 1) {
       return solve1(inputArray)
@@ -12,32 +18,38 @@ const solve = async function(part) {
   }
 }
 
-const solve1 = function(inputArray) {
+const solve1 = function(inputArray) {  
   let start = inputArray.length
+  let final = 0
+  let current = inputArray[start - 1]
   for(let i = start; i < 2020; i++) {
-    evaluateCurrent(inputArray)
+    final = evaluateCurrent(current, i)
+    current = final
   }
-  return inputArray[2019]
+  console.log(final)
+  return final
 }
 
-const solve2 = function(inputArray) {    
-  console.log(inputArray)
+const solve2 = function(inputArray) {
+  let start = inputArray.length
+  let final = 0
+  let current = inputArray[start - 1]
+  for(let i = start; i < 30000000; i++) {
+    final = evaluateCurrent(current, i)
+    current = final
+  }
+  return final
 }
 
-const evaluateCurrent = function(inputArray) {
-  let current = inputArray[inputArray.length - 1]
-  let found = false
-  for(let i = inputArray.length - 2; i >= 0; i--) {
-    if(inputArray[i] == current) {
-      current = inputArray.length - 1 - i
-      found = true
-      break
-    }
+const evaluateCurrent = function(current, iteration) {
+  let next = 0
+  if(Object.prototype.hasOwnProperty.call(foundAts, current)) {
+    next = (iteration - 1 - parseInt(foundAts[current]))
+    foundAts[current] = iteration - 1
+  } else {
+    foundAts[current] = iteration - 1
   }
-  if(!found) {
-    current = 0
-  }
-  inputArray.push(current)
+  return next
 }
 
 export default solve;
