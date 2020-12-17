5db1c0f7202db8e900c4759289ddc3cb5d3d5c6d
Bedford West <bedfordwest@gmail.com>
2020-12-16 22:46:44 -0600

    -Set up pt1

---
+++ src/day16/solution.js
@@ -0,0 +1,23 @@
+const solve = async function(part) {
+  const file = require(`../day16/INPUTS.json`)
+
+  const inputs = file.inputs
+  var inputArray = inputs.split("\n\r")
+
+  if(part == 1) {
+      return solve1(inputArray)
+  }
+  else {
+      return solve2(inputArray)
+  }
+}
+
+const solve1 = function(inputArray) {
+  console.log(inputArray)
+}
+
+const solve2 = function(inputArray) {    
+  console.log(inputArray)
+}
+
+export default solve;
