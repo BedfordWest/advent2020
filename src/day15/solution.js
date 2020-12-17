ffa9a06e54f89461dd32eeb6d6edd3992cc50064
Bedford West <bedfordwest@gmail.com>
2020-12-16 21:08:21 -0600

    -Solve pt1

--- src/day15/solution.js
+++ src/day15/solution.js
@@ -1,8 +1,8 @@
 const solve = async function(part) {
-  const file = require(`../day3/INPUTS.json`)
+  const file = require(`../day15/INPUTS.json`)
 
   const inputs = file.inputs
-  var inputArray = inputs.split("\n\r")
+  var inputArray = inputs.split(",").map((el) => parseInt(el))
 
   if(part == 1) {
       return solve1(inputArray)
@@ -13,11 +13,31 @@ const solve = async function(part) {
 }
 
 const solve1 = function(inputArray) {
-  console.log(inputArray)
+  let start = inputArray.length
+  for(let i = start; i < 2020; i++) {
+    evaluateCurrent(inputArray)
+  }
+  return inputArray[2019]
 }
 
 const solve2 = function(inputArray) {    
   console.log(inputArray)
+}
+
+const evaluateCurrent = function(inputArray) {
+  let current = inputArray[inputArray.length - 1]
+  let found = false
+  for(let i = inputArray.length - 2; i >= 0; i--) {
+    if(inputArray[i] == current) {
+      current = inputArray.length - 1 - i
+      found = true
+      break
+    }
+  }
+  if(!found) {
+    current = 0
+  }
+  inputArray.push(current)
 }
 
 export default solve;
