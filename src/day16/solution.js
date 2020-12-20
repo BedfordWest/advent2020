322eefd0774f09ebb43498a69331d44857fee160
Bedford West <bedfordwest@gmail.com>
2020-12-19 20:29:17 -0600

    -Solve pt2

--- src/day16/solution.js
+++ src/day16/solution.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 var fieldRanges = []
 var invalids = []
 
@@ -10,24 +12,36 @@ const solve = async function(part) {
   invalids = []
   const fieldArray = inputArray[0].split("\n\r")
   const nearByArray = inputArray[2].split("\n\r").slice(1)
-
+  const myArray = inputArray[1].split("\n\r").slice(1)[0].split(",").map((val) => parseInt(val))
+  
   if(part == 1) {
       return solve1(fieldArray, nearByArray)
   }
   else {
-      return solve2(inputArray)
+      return solve2(fieldArray, nearByArray, myArray)
   }
 }
 
 const solve1 = function(fieldArray, nearByArray) {
   parseFields(fieldArray)
-  findInvalids(nearByArray)
+  nearByArray = findInvalids(nearByArray)
   let sum = invalids.reduce((a,b) => a + b)
   return sum
 }
 
-const solve2 = function(inputArray) {    
-  console.log(inputArray)
+const solve2 = function(fieldArray, nearByArray, myArray) {
+  parseFields(fieldArray)
+  nearByArray = findInvalids(nearByArray)
+  let orderedFields = findFields(nearByArray)
+  orderedFields = singleOutFields(orderedFields).filter((field) => {
+    return field[0].includes("departure")
+  })
+  
+  let product = 1
+  orderedFields.forEach((field) => {
+    product *= myArray[field[1][0]]
+  })
+  return product
 }
 
 const parseFields = function(fieldArray) {
@@ -45,24 +59,69 @@ const parseFields = function(fieldArray) {
 }
 
 const findInvalids = function(nearByArray) {
-  nearByArray.forEach((vals) => {
-    vals = vals.split(",").map((valStr) => parseInt(valStr))
-    vals.forEach((val) => {
-      if(!inFieldRanges(val)) {
-        invalids.push(val)
+  return nearByArray.map((ticket) => {
+    return ticket.split(",").map((valStr) => parseInt(valStr))
+  }).filter((vals) => {
+    for(let j = 0; j < vals.length; j++) {
+      if(!inFieldRanges(vals[j])) {
+        invalids.push(vals[j])
+        return false
       }
-    })
+    }
+    return true
   })
 }
 
 const inFieldRanges = function(val) {
   for(let i = 0; i < fieldRanges.length; i++) {
     if(((val >= fieldRanges[i][1]) && (val <= fieldRanges[i][2])) ||
-       ((val >= fieldRanges[i][3]) && (val <= fieldRanges[i][4]))) {
+      ((val >= fieldRanges[i][3]) && (val <= fieldRanges[i][4]))) {
       return true
     }
   }
   return false
+}
+
+const findFields = function(nearByArray) {
+  return fieldRanges.map((fRange, i) => {
+    let possibles = []
+    for(let k = 0; k < nearByArray[0].length; k++) {
+      // Check each ticket to see if the value matches the field
+      let filteredNears = nearByArray.filter((vals) => {
+        let checkVal = vals[k]
+        if(((checkVal >= fRange[1]) && (checkVal <= fRange[2])) ||
+          ((checkVal >= fRange[3]) && (checkVal <= fRange[4]))) {
+          return true
+        } else { return false }
+      })
+      // Means every ticket worked for this field
+      if(filteredNears.length == nearByArray.length) {
+        possibles.push(k)
+      }      
+    }
+    return [fRange[0], possibles]
+  }).sort((a,b) => a[1].length - b[1].length)
+}
+
+const singleOutFields = function(fields) {
+  let newFields = []
+  let originalLength = fields.length
+  while(newFields.length != originalLength) {
+    let fieldsLen = fields.length
+    for(let j = 0; j < fieldsLen; j++) {
+      if(fields[j][1].length == 1) {
+        let foundVal = fields[j][1][0]
+        newFields.push(_.cloneDeep(fields[j]))
+        fields = fields.map((innerField, k) => {
+          let foundAt = innerField[1].indexOf(foundVal)
+          innerField[1].splice(foundAt, 1)
+          return innerField
+        }).filter((innerField) => innerField[1].length > 0)
+        break;
+      }
+    }
+  }
+  return newFields
 }
 
 export default solve;
